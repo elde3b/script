f88ac2081e672a6dda11d645f632a860e22ec0e5
elde3b <47487265+elde3b@users.noreply.github.com>
2022-02-19 01:53:03 +0200

    Add files via upload

---
+++ tester (1).py
@@ -0,0 +1,43 @@
+import facebook as fb
+from bs4 import BeautifulSoup as bs
+import requests
+
+try:
+    myUrl = requests.get('https://nitter.net/AlMosahf')
+    source = myUrl.content
+    
+    soup = bs(source, 'html.parser')
+    twi = soup.find_all('div', {'class':'tweet-content'})
+    
+    myTW = twi[1].text
+    if('pic.twitter.com' in myTW):
+        myTW = twi[2].text
+    else:
+        myTW = twi[1].text
+
+
+except:
+
+    myUrl = requests.get('https://t.qbool.fr/AlMosahf')
+    source = myUrl.content
+
+    soup = bs(source, 'html.parser')
+
+    twi = soup.find_all('div', {'class':'tweet-content'})
+    myTW = twi[1].text
+    if('pic.twitter.com' in myTW):
+        myTW = twi[2].text
+    else:
+        myTW = twi[1].text
+
+
+############################################
+access_token = "EAADMxZAMrZCMYBAHARd62TgDQ0CVmgaVlnrZCapZBxWltB13ZADfGTzAsBH8Pd0duzPFqTxusm3YCNyyYZBS0r4ZB1by7k19jPsjog4HEWZABAJg3pu0fZApUkUWnBNSapbSVVg6urGZAXuESG6Vd9fZAKDDCQMMK4laKL03TNTULUUmgZDZD"
+############################################
+
+asafb = fb.GraphAPI(access_token)
+
+asafb.put_object("me", "feed", message = myTW)
+
+
+
